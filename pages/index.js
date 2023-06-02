788ec32f9dca47d29294bcc21220a92332d72d9e
정은선 <jeong-eunseon@jeong-eunseon-ui-MacBookAir.local>
2023-06-03 01:52:52 +0900

    fix: getStaticProps으로 정적 생성

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,32 @@ import { useEffect, useState } from "react";
 import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import axios from "../lib/axios";
-
 import { Column } from "@/components/Flex";
 import Page from "@/components/Page";
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
-
-  const getProducts = async () => {
-    const response = await axios.get("/products");
-    const nextProduct = response.data.results;
-    setProducts(nextProduct);
+//정적 생성을 할 때 next.js가 실행 할 함수
+export async function getStaticProps() {
+  const response = await axios.get("/products");
+  const products = response.data.results;
+  return {
+    props: {
+      products,
+    },
   };
+}
 
-  useEffect(() => {
-    getProducts();
-  }, []);
+export default function Home({ products }) {
+  // const [products, setProducts] = useState([]);
+
+  // const getProducts = async () => {
+  //   const response = await axios.get("/products");
+  //   const nextProduct = response.data.results;
+  //   setProducts(nextProduct);
+  // };
+
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
 
   return (
     <>
